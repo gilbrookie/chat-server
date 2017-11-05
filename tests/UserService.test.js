1b79dba38eaad17959657e773614d964b8da2635
James Gilbrook <james@gilbrook.ca>
2017-11-05 12:06:49 -0500

    Fix UserService test to work with db integration
    Update docker config to define a db host when running in docker, defaulting to localhost for tests.

--- tests/UserService.test.js
+++ tests/UserService.test.js
@@ -3,7 +3,7 @@ const UserService = require('../lib/UserService');
 const { User } = require('../lib/db');
 
 describe('UserService Tests', () => {
-  test('Create a user', () => {
+  test('Create a user, with a uuid', () => {
     const obj = {
       id: uuidV4(),
       email: `test+${uuidV4()}@example.com`,
@@ -19,26 +19,51 @@ describe('UserService Tests', () => {
       });
   });
 
+  test('create a user without a uuid (auto-generate)', () => {});
+  test('create user with email that already exists', () => {});
+
   test('Update a user', () => {
     const obj = {
       id: uuidV4(),
       email: `test+${uuidV4()}@example.com`,
+      name: 'Test User',
     };
-    return UserService.update(obj.id, { email: obj.email })
+    const newEmail = `test+MODIFIED+${uuidV4()}@example.com`;
+
+    return UserService.create(obj)
+      .then(() => UserService.update(obj.id, { email: newEmail }))
+      .then(() => UserService.findOne(obj.id))
       .then((res) => {
-        expect(res).toHaveProperty('id', obj.id);
-        expect(res).toHaveProperty('updateData', { email: obj.email });
+        expect(res.id).toEqual(obj.id);
+        expect(res.email).toEqual(newEmail);
       });
   });
 
+  test('update a user that doesn\'t exist', () => {});
+  test('update a user\'s email to one that already exists', () => {});
+  test('update a user\'s id to one that already exists', () => {});
+
   test('Find a user', () => {
-    const id = uuidV4();
-    return UserService.findOne(id)
-      .then(res => expect(res).toHaveProperty('id', id));
+    const obj = {
+      id: uuidV4(),
+      email: `test+${uuidV4()}@example.com`,
+      name: 'Test User',
+    };
+    return UserService.create(obj)
+      .then(user => UserService.findOne(user.id))
+      .then(res => expect(res.id).toEqual(obj.id));
   });
 
   test('Delete a user', () => {
-    return UserService.destroy(uuidV4())
-      .then(res => expect(res).toBe(null));
+    const obj = {
+      id: uuidV4(),
+      email: `test+${uuidV4()}@example.com`,
+      name: 'Test User',
+    };
+    return UserService.create(obj)
+      .then(user => UserService.destroy(user.id))
+      .then(res => expect(res).toEqual(1))
+      .then(() => UserService.findOne(obj.id))
+      .then(res1 => expect(res1).toBeNull());
   });
 });
