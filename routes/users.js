52ee10344bc8f5626fc354f3a0a826f134e50397
James <james@gilbrook.ca>
2017-11-04 22:11:32 -0400

    Add support for creating Usr entries in the database; Add rough-ins for other methods

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,38 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const UserService = require('../lib/UserService');
+
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res) => {
+  req.log.info('Calling Users GetAll()');
+  return UserService.findAll()
+    .then(users => res.send(users));
+});
+
+
+router.get('/:userId/', (req, res) => {
+  req.log.info('Calling Users.getOne()');
+  return UserService.findOne(req.params.userId)
+    .then(user => res.send(user));
+});
+
+router.post('/', (req, res) => {
+  req.log.info('Creating a new user');
+  return UserService.create(req.body)
+    .then(user => res.send(user));
+});
+
+router.put('/:userId/', (req, res) => {
+  req.log.info({ id: req.params.userId }, 'Updating User');
+  return UserService.update(req.params.userId, req.body)
+    .then(user => res.send(user));
+});
+
+router.delete('/:userId/', (req, res) => {
+  req.log.info({ id: req.params.userId }, 'Deleteing User');
+  return UserService.delete(req.params.userId)
+    .then(res.send(204));
 });
 
 module.exports = router;
--- tests/UserService.test.js
+++ tests/UserService.test.js
@@ -1,19 +1,44 @@
+const uuidV4 = require('uuid/v4');
 const UserService = require('../lib/UserService');
+const { User } = require('../lib/db');
 
 describe('UserService Tests', () => {
   test('Create a user', () => {
-    expect(UserService.create()).toEqual(true);
+    const obj = {
+      id: uuidV4(),
+      email: `test+${uuidV4()}@example.com`,
+      name: 'Test User',
+    };
+
+    return UserService.create(obj)
+      .then((res) => {
+        expect(res).toBeInstanceOf(User);
+        expect(res.id).toEqual(obj.id);
+        expect(res.email).toEqual(obj.email);
+        expect(res.name).toEqual(obj.name);
+      });
   });
 
   test('Update a user', () => {
-    expect(UserService.update()).toEqual(true);
+    const obj = {
+      id: uuidV4(),
+      email: `test+${uuidV4()}@example.com`,
+    };
+    return UserService.update(obj.id, { email: obj.email })
+      .then((res) => {
+        expect(res).toHaveProperty('id', obj.id);
+        expect(res).toHaveProperty('updateData', { email: obj.email });
+      });
   });
 
   test('Find a user', () => {
-    expect(UserService.findOne()).toEqual(true);
+    const id = uuidV4();
+    return UserService.findOne(id)
+      .then(res => expect(res).toHaveProperty('id', id));
   });
 
   test('Delete a user', () => {
-    expect(UserService.destroy()).toEqual(true);
+    return UserService.destroy(uuidV4())
+      .then(res => expect(res).toBe(null));
   });
 });
